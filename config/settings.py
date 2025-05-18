c51b1eb49b070b9df3fb4832349a55ca076494fc
shoxista-baxtiyorovna <shoxistairiskulova@gmail.com>
2025-05-18 14:54:57 +0300

    swagger is generated

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'users.apps.UsersConfig',
