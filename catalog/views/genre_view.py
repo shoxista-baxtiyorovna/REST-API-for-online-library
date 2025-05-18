b90c465142775f2b40d0b45f6616ceeafd21ae6f
shoxista-baxtiyorovna <shoxistairiskulova@gmail.com>
2025-05-18 15:34:32 +0300

    comments are removed

--- catalog/views/genre_view.py
+++ catalog/views/genre_view.py
@@ -6,11 +6,3 @@ from catalog.serializers import GenreSerializer, AuthorSerializer
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-
-
-    # @action(detail=True, methods=['get'], url_path='books', pagination_class=GenreBookPagination)
-    # def books(self, request, pk=None):
-    #     books = Book.objects.filter(genre__id=pk)
-    #     page = self.paginate_queryset(books)
-    #     serializer = AuthorBookSerializer(page, many=True) if page is not None else AuthorBookSerializer(books, many=True)
-    #     return self.get_paginated_response(serializer.data) if page is not None else Response(serializer.data)
